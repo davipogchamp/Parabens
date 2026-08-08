c68e18bbd7c65b1311848d7995c376b54beafdb5
davipogchamp <daviassini04@gmail.com>
2026-08-08 01:28:09 -0300

    Adicionar arquivos de projeto.

---
+++ Parabens/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Parabens.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
